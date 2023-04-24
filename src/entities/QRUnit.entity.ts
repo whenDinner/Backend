69a0c322dd24585ec4d85c6ba03881bc675032ef
th-release <cth.release@gmail.com>
2023-04-24 12:24:32 +0900

    feat: update Entity

--- src/entities/QRUnit.entity.ts
+++ src/entities/QRUnit.entity.ts
@@ -1,7 +1,13 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: 'QRUnitEntity' })
 export default class QRUnitEntity {
   @PrimaryGeneratedColumn("uuid")
   uuid: string;
+
+  @Column({ name: "data", type: "text", nullable: false })
+  data: string;
+
+  @Column({ name: "type", type: "varchar", length: 16, nullable: false })
+  type: string;
 }
