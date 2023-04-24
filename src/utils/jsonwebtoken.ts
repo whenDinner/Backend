d5deaa9a599546fef0d1221a48ade4c5d7d691d8
th-release <cth.release@gmail.com>
2023-04-24 17:53:19 +0900

    feat: jsonwebtoken utils function

---
+++ src/utils/jsonwebtoken.ts
@@ -0,0 +1,29 @@
+import { sign, verify } from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
+import { UserToken } from './interfaces';
+
+class jsonwebtoken {
+  static sign(user: UserToken, configService: ConfigService) {
+    return sign(user, configService.get<string>('JWT_SECRET'), {
+      expiresIn: '7d',
+    });
+  }
+
+  static verify(token: string, configService: ConfigService) {
+    try {
+      const decode = verify(token, configService.get<string>('JWT_SECRET')) as UserToken;
+      return {
+        success: true,
+        message: '',
+        user: decode
+      };
+    } catch (err) {
+      return {
+        success: false,
+        message: err.message,
+      };
+    }
+  }
+}
+
+export default jsonwebtoken;
