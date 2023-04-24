4a561da0736c09eede50f91d5f6bd99f6f91c118
th-release <cth.release@gmail.com>
2023-04-25 08:30:41 +0900

    feat: import AccountEntity

--- src/qrcode/qrcode.module.ts
+++ src/qrcode/qrcode.module.ts
@@ -5,9 +5,10 @@ import { QrcodeController } from './qrcode.controller';
 import { QrcodeService } from './qrcode.service';
 import QRUnitEntity from 'src/entities/QRUnit.entity';
 import QRIterEntity from 'src/entities/QRIter.entity';
+import AccountEntity from 'src/entities/account.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([QRUnitEntity, QRIterEntity]),
+  imports: [TypeOrmModule.forFeature([AccountEntity, QRUnitEntity, QRIterEntity]),
     RedisModule
   ],
   exports: [TypeOrmModule],
