c44fb928660d6c589d26f6f13c4d1c7bf8e68056
th-release <cth.release@gmail.com>
2023-04-24 17:57:58 +0900

    feat: add Column (student_id, student_name)

--- src/entities/outgo.entity.ts
+++ src/entities/outgo.entity.ts
@@ -1,9 +1,19 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: 'outgoEntity' })
 export default class OutgoEntity {
   @PrimaryGeneratedColumn("uuid")
   uuid: string;
 
-  
+  @Column({ name: "student_id", type: "varchar", length: 6, nullable: false })
+  student_id: string;
+
+  @Column({ name: "student_name", type: "varchar", length: 6, nullable: false })
+  student_name: string;
+
+  @Column({ name: "type", type: "int", nullable: false })
+  type: number;
+
+  @CreateDateColumn({ name: "createdAt", nullable: false })
+  createdAt: Date;
 }
