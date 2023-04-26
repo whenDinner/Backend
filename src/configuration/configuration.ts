dc7cb63aa74747f87e5fbe65d3583cebe52f4a9d
th-release <cth.release@gmail.com>
2023-04-26 19:51:25 +0900

    config: env configuration

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -1,4 +1,5 @@
 export default () => ({
+  JWT_SECRET: process.env.JWT_SECRET,
   DATABASE_HOST: process.env.DATABASE_HOST,
   DATABASE_PORT: parseInt(process.env.DATABASE_PORT) || 8000,
   DATABASE_USERNAME: process.env.DATABASE_USERNAME,
@@ -7,6 +8,11 @@ export default () => ({
   TYPEORM_SYBCHRONIZE: process.env.MODE === "DEV" ? true : false,
   MODE: process.env.MODE,
   REDIS_HOST: process.env.REDIS_HOST,
-  REDIS_PORT: parseInt(process.env.REDIS_PORT) || 6379,
-  REDIS_TTL: parseInt(process.env.REDIS_TTL) || 100000
+  REDIS_PORT: parseInt(process.env.REDIS_PORT) || 8080,
+  REDIS_TTL: parseInt(process.env.REDIS_TTL) || 100000,
+  GBSW_CLIENT_ID: process.env.GBSW_CLIENT_ID,
+  GBSW_REDIRECT_URI: process.env.GBSW_REDIRECT_URI,
+  GBSW_SCOPES: process.env.GBSW_SCOPES,
+  GBSW_PUBKEY_URL: process.env.GBSW_PUBKEY_URL,
+  GBSW_ISSUER: process.env.GBSW_ISSUER
 })
