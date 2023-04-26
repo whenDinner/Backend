dd427b8c901ecf957be71f1b50eb9d1c5a58005c
th-release <cth.release@gmail.com>
2023-04-26 19:52:48 +0900

    feat: token interfaces

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,12 +1,60 @@
+type UserType = 0 | 1 | 2
+
+export interface oidcToken {
+  iss: string,
+  sub: string,
+  aud: string,
+  nonce: any,
+  data: {
+    id: number,
+    login: string,
+    nickname: string,
+    createdAt: Date,
+    type: UserType,
+    classInfo: {
+      grade: number,
+      class: number,
+      number: number
+    },
+    dormitory?: {
+      room: string
+    },
+    gender: 'M' | 'F'
+    phone: string,
+    fullname: string
+  },
+  iat: number,
+  exp: number
+}
+
+export interface oidcTokenData {
+  id: number,
+  login: string,
+  nickname: string,
+  createdAt: Date,
+  type: UserType,
+  classInfo: {
+    grade: number,
+    class: number,
+    number: number
+  },
+  dormitory?: {
+    room: string
+  },
+  gender: 'M' | 'F'
+  phone: string,
+  fullname: string
+}
+
 export interface UserToken {
-  uuid: string,
+  login: string,
+  nickname: string,
+  student_id: string,
   grade: number,
   class: number,
   number: number,
   roomNumber: number,
-  name: string,
-  type: number,
-  isExit: boolean,
-  isOuting: boolean,
-  createdAt: Date
+  fullname: string,
+  gender: 'M' | 'F',
+  type: UserType,
 }
