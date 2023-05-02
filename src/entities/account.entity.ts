7102c342d1775a25821532b4426698dca6b992d1
th-release <cth.release@gmail.com>
2023-05-02 19:52:58 +0900

    feat: entity

--- src/entities/account.entity.ts
+++ src/entities/account.entity.ts
@@ -8,38 +8,38 @@ export default class AccountEntity {
   @Column({ name: 'login', type: 'varchar', length: 36, nullable: false, unique: true })
   login: string;
 
-  @Column({ name: 'nickname', type: 'varchar', length: 36, nullable: false })
-  nickname: string;
+  @Column({ name: 'nickname', type: 'varchar', length: 36, nullable: true })
+  nickname?: string | null;
 
   @Column({ name: 'student_id', type: 'varchar', length: 6, nullable: false })
-  student_id: string;
+  student_id?: string | null;
 
-  @Column({ name: 'grade', type: 'int' })
-  grade: number;
+  @Column({ name: 'grade', type: 'int', nullable: true })
+  grade?: number | null;
 
-  @Column({ name: 'class', type: 'int' })
-  class: number;
+  @Column({ name: 'class', type: 'int', nullable: true })
+  class?: number | null;
 
-  @Column({ name: 'number', type: 'int' })
-  number: number;
+  @Column({ name: 'number', type: 'int', nullable: true })
+  number?: number | null;
 
-  @Column({ name: 'roomNumber', type: 'varchar', length: 4 })
-  roomNumber: string;
+  @Column({ name: 'roomNumber', type: 'varchar', length: 4, nullable: true })
+  roomNumber?: string | null;
 
   @Column({ name: 'fullname', type: 'varchar', length: 6 })
   fullname: string;
 
-  @Column({ name: 'gender', type: "varchar", length: 1, nullable: false })
-  gender: "M" | "F";
+  @Column({ name: 'gender', type: "varchar", length: 1, nullable: true })
+  gender?: "M" | "F" | null;
 
   @Column({ name: 'type', type: 'int', nullable: false })
   type: 0 | 1 | 2;
 
-  @Column({ name: 'isExit', type: 'bool', nullable: true })
-  isExit: boolean;
+  @Column({ name: 'isReturn', type: 'bool', nullable: true })
+  isReturn?: boolean | null ;
 
   @Column({ name: 'isOuting', type: 'bool', nullable: true })
-  isOuting: boolean;
+  isOuting?: boolean | null;
   
   @CreateDateColumn({ name: 'createdAt' })
   createdAt: Date;
