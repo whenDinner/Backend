218375b0999ccb3a8559bb1f805f9b413c081578
th-release <cth.release@gmail.com>
2023-05-03 19:17:08 +0900

    feat: Inject Repositorys

--- src/qrcode/qrcode.service.ts
+++ src/qrcode/qrcode.service.ts
@@ -1,16 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Request, Response } from 'express';
 import QRUnitEntity from 'src/entities/QRUnit.entity';
 import AccountEntity from 'src/entities/account.entity';
 import { toString, toBuffer, toDataURL } from 'qrcode';
 import { Repository } from 'typeorm';
+import QRIterEntity from 'src/entities/QRIter.entity';
+import jsonwebtoken from 'src/utils/jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class QrcodeService {
   constructor(
+    @InjectRepository(AccountEntity)
+    private accountRepository: Repository<AccountEntity>,
     @InjectRepository(QRUnitEntity)
-    private qrUnitRepository: Repository<QRUnitEntity>
+    private qrUnitRepository: Repository<QRUnitEntity>,
+    @InjectRepository(QRIterEntity)
+    private qrIterRepository: Repository<QRIterEntity>,
+    @Inject(CACHE_MANAGER)
+    private configService: ConfigService
   ) {};
 
   async get(req: Request, res: Response) {
@@ -69,12 +78,21 @@ export class QrcodeService {
   async verify(req: Request, res: Response) {
     const auth = req.headers.authorization.split(' ')[1];
     const { data, type } = req.body;
-    if (!auth || !type)       
+    console.log(auth, data, type)
+    if (!auth || !type || !data)
       return res.status(400).json({ 
         success: false,
         message: 'required data'
       })
+
     try {
+      const user = jsonwebtoken.verify(auth, this.configService.get('JWT_SECRET'));
+      if (!user.success) 
+        return res.status(401).json({
+          success: false,
+          message: 'invaild token'
+        })
+
       const qrData = await this.qrUnitRepository.findOne({
         where: {
           data: data,
@@ -88,7 +106,37 @@ export class QrcodeService {
           message: 'invaild QR Data'
         })
       else {
-        
+        const date = new Date()
+        date.setHours(0, 0, 0, 0);
+
+        const qrIter = await this.qrIterRepository.findOne({
+          where: { 
+            student_id: user.data.student_id,
+            student_name: user.data.fullname,
+            data: qrData.data,
+            type: qrData.type,
+            createdAt: date 
+          } 
+        });
+
+        if (!qrIter) {
+          await this.qrIterRepository.insert({
+            student_id: user.data.student_id,
+            student_name: user.data.fullname,
+            status: true,
+            data: qrData.data,
+            type: qrData.type,
+            createdAt: date
+          })
+        } else {
+          qrIter.status = !qrIter.status
+          await this.qrIterRepository.save(qrIter);
+        }
+
+        return res.status(200).json({
+          success: true,
+          message: '정상적으로 처리 되었습니다.'
+        })
       }
     } catch(err) {
       return res.status(500).json({
