fcf8cdb0e635ea88f09f91525167895c6d7e1e7f
th-release <cth.release@gmail.com>
2023-05-06 13:35:46 +0900

    feat: add Routes

--- src/community/community.controller.ts
+++ src/community/community.controller.ts
@@ -5,8 +5,23 @@ import { CommunityService } from './community.service';
 export class CommunityController {
   constructor(private communityService: CommunityService) {};
 
-  @Post('/comment')
+  @Post('/comment/post')
   postComment(@Req() req, @Res() res) {
-    
+    return this.communityService.postComment(req, res);
+  }
+
+  @Post('/comment/reply')
+  replyComment(@Req() request, @Res() response) {
+    return this.communityService.replyComment(request, response);
+  }
+
+  @Post('/post/insert')
+  insertPost(@Req() request, @Res() response) {
+    return this.communityService.insertPost(request, response);
+  }
+
+  @Post('/post/delete')
+  deletePost(@Req() request, @Res() response) {
+    return this.communityService;
   }
 }
