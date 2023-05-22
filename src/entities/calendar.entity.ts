354dcf4e4d6205ab9349e747bee27de7ac94fc91
th-release <cth.release@gmail.com>
2023-05-23 08:45:35 +0900

    feat: calendarEntity

---
+++ src/entities/calendar.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "calendarEntity" })
+export default class CalendarEntity {
+  @PrimaryGeneratedColumn("uuid")
+  uuid: string;
+
+  @Column({ name: 'type', type: 'varchar', length: 2, nullable: false })
+  type: "잔류" | "귀가";
+
+  @Column({ name: 'date', type: "date", unique: true, nullable: false })
+  date: Date;
+}
