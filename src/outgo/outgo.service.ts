bfe3d05dce6d6464f3216031802313871146f634
th-release <cth.release@gmail.com>
2023-05-23 08:47:41 +0900

    feat: updateCalendar, setOutgo

--- src/outgo/outgo.service.ts
+++ src/outgo/outgo.service.ts
@@ -1,4 +1,139 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Request, Response } from 'express';
+import AccountEntity from 'src/entities/account.entity';
+import CalendarEntity from 'src/entities/calendar.entity';
+import OutgoEntity from 'src/entities/outgo.entity';
+import jsonwebtoken from 'src/utils/jsonwebtoken';
+import { isValidType } from 'src/utils/typeChecks';
+import { MoreThan, Repository } from 'typeorm';
 
 @Injectable()
-export class OutgoService {}
+export class OutgoService {
+  constructor(
+    @InjectRepository(AccountEntity)
+    private accountRepository: Repository<AccountEntity>,
+    @InjectRepository(OutgoEntity)
+    private outgoRepository: Repository<OutgoEntity>,
+    @InjectRepository(CalendarEntity)
+    private calendarRepository: Repository<CalendarEntity>,
+    private readonly configService: ConfigService
+  ) {};
+
+  async updateCalendar(req: Request, res: Response) {
+    const { xlsx } = req.body;
+    const workbook = xlsx.read(xlsx, { type: 'buffer' });
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+
+    const excelData = xlsx.utils.sheet_to_json(worksheet) as { date: Date, type: "잔류" | "귀가" }[];
+    for (const data of excelData) {
+      try {
+        const xlsxData = await this.calendarRepository.findOne({
+          where: {
+            date: data.date
+          }
+        })
+        
+        if (!xlsxData) {
+          await this.calendarRepository.insert({
+            date: data.date,
+            type: data.type
+          })
+          .catch(() => { throw ({ status: 400, message: 'type Error' }) })
+        } else {
+          await this.calendarRepository.update({ date: data.date }, {
+            type: data.type
+          })
+          .catch(() => { throw ({ status: 400, message: 'type Error' }) })
+        }
+      } catch (err) {
+        return res.status(err.status).json({
+          success: false,
+          message: err.message
+        })
+      }
+    }
+    
+    return res.status(200).json({
+      success: true,
+      message: ''
+    })
+  }
+
+  async setOutgo(req: Request, res: Response) {
+    const { dotw, type, reason } = req.body;
+
+    const token = req.headers.authorization.split(' ')[1];
+    const verify = jsonwebtoken.verify(token, this.configService.get('JWT_SECRET'));
+
+    const validDotwType = ['금', '토', '일']
+    const validOutgoType = ['오전외출', '오후외출', '외출', '외박']
+
+    const user = await this.accountRepository.findOne({
+      where: { login: verify.data.login }
+    })
+
+    try {
+      if (!type) throw ({ status: 400, message: 'required type' })
+      if (!token || !verify || !user) throw ({ status: 400, message: 'invaild token' })
+      if (!isValidType(dotw, validDotwType)) throw ({ status: 400, message: 'invaild dotwType' })
+      if (!isValidType(type, validOutgoType)) throw ({ status: 400, message: 'invaild outgoType' })
+      if (dotw === '금' && type !== '외박') throw ({ status: 400, message: '금요일엔 외박 이외엔 다른것 선택 불가' });
+      if (dotw === '토' && type === '오전외출') throw ({ status: 400, message: '토요일엔 오전외출 불가' });
+      if (dotw === '일' && type === '외박') throw ({ status: 400, message: '일요일엔 외박 불가' })
+    } catch(err) {
+      return res.status(err.status).json({
+        success: false,
+        message: err.message
+      })
+    }
+
+    try {
+      const calendar = await this.calendarRepository.findOne({
+        where: {
+          date: MoreThan(new Date()),
+        },
+        order: {
+          date: 'ASC',
+        },
+      });
+
+      if (!calendar) throw({ status: 500, message: '더이상의 잔류, 귀가 표가 없습니다. 관리자에게 문의해주세요' })
+  
+      const userAnswer = await this.outgoRepository.findOne({
+        where: {
+          outgoDate: calendar.date,
+          user_id: user.uuid
+        }
+      })
+
+      if (userAnswer) {
+        await this.outgoRepository.update({user_id: userAnswer.user_id}, {
+          fri_out: dotw === "금" ? true : false,
+          sat_pm: dotw === "토" && type === "오후외출" ? true : false,
+          sun_am: dotw === "일" && type === "오전외출" ? true : false,
+          sun_pm: dotw === "일" && type === "오후외출" ? true : false,
+          sun: dotw === "일" && type === "외출" ? true : false,
+          reason
+        })
+      } else {
+        await this.outgoRepository.insert({
+          user_id: user.uuid,
+          fri_out: dotw === "금" ? true : false,
+          sat_pm: dotw === "토" && type === "오후외출" ? true : false,
+          sun_am: dotw === "일" && type === "오전외출" ? true : false,
+          sun_pm: dotw === "일" && type === "오후외출" ? true : false,
+          sun: dotw === "일" && type === "외출" ? true : false,
+          reason
+        })
+      }
+    } catch(err) {
+      return res.status(err.status).json({
+        success: false,
+        message: err.message
+      })
+    }
+  }
+}
