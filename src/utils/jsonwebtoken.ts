a9b1ab81ba45b801af27c873baa2eb1f2a020ff2
th-release <cth.release@gmail.com>
2023-05-22 12:24:07 +0900

    feat: sign code change

--- src/utils/jsonwebtoken.ts
+++ src/utils/jsonwebtoken.ts
@@ -1,18 +1,10 @@
 import { sign, verify } from 'jsonwebtoken';
-import { ConfigService } from '@nestjs/config';
 import { UserToken, oidcTokenData } from './interfaces';
 
 class jsonwebtoken {
   static sign(user: oidcTokenData, secret: string) {
     return sign({
       login: user.login,
-      nickname: user.nickname,
-      student_id: String(user.classInfo.grade) + String(user.classInfo.class) + (user.classInfo.number < 10 ? '0' + String(user.classInfo.number) : String(user.classInfo.number)),
-      grade: user.classInfo.grade,
-      class: user.classInfo.class,
-      number: user.classInfo.number,
-      roomNumber: user.dormitory.room,
-      fullname: user.fullname,
       gender: user.gender,
       type: user.type,
     }, secret, {
